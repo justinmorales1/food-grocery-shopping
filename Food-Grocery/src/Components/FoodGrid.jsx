5b48e27db97b993799cd7cea9cce6ea6335187b6
Nicolas Alvarez Mondragon <nicogranuja@hotmail.com>
2019-11-28 11:20:49 -0600

    Fixed all warnings and errors

--- Food-Grocery/src/Components/FoodGrid.jsx
+++ Food-Grocery/src/Components/FoodGrid.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
-import {Grid} from '@material-ui/core'
-import FoodCard from './FoodItemCard'
+import { withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import FoodCard from './FoodItemCard';
 
 const styles = {
-    grid:{
-        width: '100vw'
-    }
-}
+  grid: {
+    width: '100vw'
+  }
+};
 
-
-class FoodGrid extends React.Component{
-    styles = this.props.styles;
-    render() {
-        const classes = this.props
-        const foodCards = classes.foodLists.map((food) =>
-           <FoodCard price={food.price} imgURL={food.url} Title={food.title} Description={food.description} youtubeId ={food.youtubeId}/>
-        );
-        return(
-            <Grid style={styles.grid} container justify="center">
-                {foodCards}
-            </Grid>
-        );
-    }
+class FoodGrid extends React.Component {
+  styles = this.props.styles;
+  render() {
+    const classes = this.props;
+    const foodCards = classes.foodLists.map((food, index) => (
+      <FoodCard
+        key={index}
+        price={food.price}
+        imgURL={food.url}
+        Title={food.title}
+        Description={food.description}
+      />
+    ));
+    return (
+      <Grid style={styles.grid} container justify='center'>
+        {foodCards}
+      </Grid>
+    );
+  }
 }
 
 FoodGrid.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(FoodGrid);
